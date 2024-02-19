e75154d9d71c9b01261a4f5efd6b6cbdd59d473d
Luke <verdant.luke@gmail.com>
2024-02-19 14:04:19 -0500

    test cases

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -20,6 +20,11 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
+  it("should return false if the matrix is empty", function() {
+    const result = wordSearch([]);
+    assert.isFalse(result);
+  });
+
   it("should return true if the word is present vertically", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
@@ -37,7 +42,39 @@ describe("#wordSearch()", function() {
   });
 
 
-  it("should return true if the word is present horizontally", function() {
+  it("should return false if the matrix contains a non-array row", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'F', 'L', 'D'],
+      42,
+      ['H', 'M', 'J', 'T', 'E', 'A', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'N', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'K', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRANK');
+
+    assert.isFalse(result);
+  });
+
+  it("should return false if the matrix contains a row with a different number of chars", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'F', 'D'],
+      ['W', 'H', 'C', 'S', 'Y', 'N', 'R', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'A', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'N', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'K', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRANK');
+
+    assert.isFalse(result);
+  });
+
+  it("should return true if the first is present horizontally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -51,5 +88,37 @@ describe("#wordSearch()", function() {
     ], 'SEINFELD');
 
     assert.isTrue(result);
+  });
+
+  it("should return false if the element is more than 1 character", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'ELAINE', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD');
+
+    assert.isFalse(result);
+  });
+
+  it("should return false if the element is not a string", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 42, 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD');
+
+    assert.isFalse(result);
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,6 +1,24 @@
 const transpose = require('./transpose');
 
 const wordSearch = (letters, word) => {
+
+  // check valid, non-empty matrix
+  // check that it has rows
+  if (!Array.isArray(letters) || letters.length === 0) return false;
+  // check that first row is Array
+  if (!Array.isArray(letters[0])) return false;
+  // check that the first row has cols
+  const numCols = letters[0].length;
+  if (numCols === 0) return false;
+
+  //checking to see if each row is an array and has the same amount of chars
+  for (const row of letters) {
+    if (!Array.isArray(row) || row.length !== numCols) return false;
+    for (const ele of row) {
+      if (typeof ele !== "string" || ele.length > 1) return false;
+    }
+  }
+
   const horizontalJoin = letters.map(ls => ls.join(''));
 
   for (const l of horizontalJoin) {
@@ -20,14 +38,14 @@ const wordSearch = (letters, word) => {
 module.exports = wordSearch;
 
 
-const result = wordSearch([
-  ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-  ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-  ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-  ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-  ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
-  ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-  ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
-  ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
-  ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-], 'FRANK');
+// wordSearch([
+//   ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//   ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+//   ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//   ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+//   ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+//   ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+//   ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+//   ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+//   ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+// ], 'FRANK');
